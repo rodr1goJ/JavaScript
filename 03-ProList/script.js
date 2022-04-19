13d6c4168bc07675256aaf477834e07d0dd9bfb7
rodrigo0101 <contatorjneves@gmail.com>
2022-04-19 01:24:33 -0300

    ListaV2

--- 03-ProList/script.js
+++ 03-ProList/script.js
@@ -1,9 +1,23 @@
-function adicionar(){
-   
-    let text = document.getElementById("texto").value;
-    let list = document.getElementById("lista").innerHTML;
 
-    list += text
-    document.getElementById("lista").innerHTML = list;
+
+class Produto{
+    //funcao construtora denytro do projeto
+    constructor(){
+
+        this.id=0;//dentro desta classe vai ter um atributo id
+        this.nomeProduto='';//dentro desta classe vai ter um atributo nome
+        this.valor=0;//dentro desta classe vai ter um atributo valor
+    }
+
+    //definindo todos os metodos
+    adicionar(){
+        alert('metodo adicionar apertado');
+    }
+    excluir(){
+        alert('item deletado');
+    }
 
 }
+var produto = new Produto();//criou um objeto do tipo produto
+
+
