b8ba0dbc949ae95258a4de06fa22ed6a409125f0
rodrigo0101 <contatorjneves@gmail.com>
2022-04-22 18:40:08 -0300

    ListaV3

--- 03-ProList/script.js
+++ 03-ProList/script.js
@@ -3,17 +3,54 @@
 class Produto{
     //funcao construtora denytro do projeto
     constructor(){
-
-        this.id=0;//dentro desta classe vai ter um atributo id
-        this.nomeProduto='';//dentro desta classe vai ter um atributo nome
-        this.valor=0;//dentro desta classe vai ter um atributo valor
+        this.id = 1;//dentro desta classe vai ter um atributo id
+        this.arrayProdutos = [];
     }
 
     //definindo todos os metodos
-    adicionar(){
-        alert('metodo adicionar apertado');
+    salvar(){
+        
+        let produto = this.lerDados();// vai receber os dados do campo 
+        
+        if(this.validaCampos(produto)){
+            alert('salvo')
+        }
+        
+
+        console.log(produto); 
+
     }
-    excluir(){
+
+    lerDados(){
+        let produto = {}//quando coloca chabves se trata de um obj
+        
+        produto.id =  this.id;
+        produto.nomeProduto = document.getElementById('produto').value;
+        produto.preco = document.getElementById('preco').value;
+        
+        return produto;
+    }
+
+    validaCampos(produto){
+        let msg = ''; 
+
+        if(produto.nomeProduto == ''){
+            msg += 'Informe o nome do produto \n';
+        }
+        if(produto.preco == ''){
+            msg += 'Informe o preco do produto \n'
+        }
+
+        if(msg !=''){
+            alert(msg);
+            return false
+        }
+        return true
+
+    }
+
+    cancelar(){
+
         alert('item deletado');
     }
 
